ef0870a2081be024f23e8cdee8d863a8d3eaf63c
Mariya <mariya.podosinova@gmail.com>
2020-05-14 12:35:07 +0100

    Updates

---
+++ precache-manifest.9af22ef7e07db140104f12b82112afc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d19d33c86dbc2cd6c2f78641544e7631",
+    "url": "/lottery/index.html"
+  },
+  {
+    "revision": "e10be0a03d1ad52383b3",
+    "url": "/lottery/static/css/main.a7be5768.chunk.css"
+  },
+  {
+    "revision": "f5d85bdf14663077e057",
+    "url": "/lottery/static/js/2.94356501.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lottery/static/js/2.94356501.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e10be0a03d1ad52383b3",
+    "url": "/lottery/static/js/main.55667ae1.chunk.js"
+  },
+  {
+    "revision": "9597919e62f935278ea0",
+    "url": "/lottery/static/js/runtime-main.823fbc65.js"
+  }
+]);
