76f7a75feee1783e0899cc04477968d20436ca7c
Mariya <mariya.podosinova@gmail.com>
2020-05-14 12:05:02 +0100

    final for 6 balls

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Ball from "./Ball";
+import Lottery from "./Lottery";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Ball />
+      <Lottery />
     </div>
   );
 }
